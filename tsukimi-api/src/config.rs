d194950e631fa54ae3218a8b92112d223f361b20
FlenderGit <tristan.deloeil@gmail.com>
2025-08-10 17:32:56 +0200

    ADD: start api & web

--- tsukimi-api/src/config.rs
+++ tsukimi-api/src/config.rs
@@ -6,6 +6,56 @@ use serde::Deserialize;
 pub struct Configuration {
     #[getset(get_copy = "pub")]
     port: u16,
+    #[getset(get = "pub")]
+    database: DatabaseConfiguration,
+    #[getset(get = "pub")]
+    env: Environment,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Environment {
+    Development,
+    Production,
+    Testing,
+    Staging,
+}
+impl Environment {
+    pub fn is_production(&self) -> bool {
+        matches!(self, Environment::Production)
+    }
+}
+impl<'de> Deserialize<'de> for Environment {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        match s.as_str() {
+            "development" | "dev" => Ok(Environment::Development),
+            "testing" | "test" => Ok(Environment::Testing),
+            "staging" => Ok(Environment::Staging),
+            "production" | "prod" => Ok(Environment::Production),
+            _ => Err(serde::de::Error::custom(format!(
+                "Unknown environment: {}",
+                s
+            ))),
+        }
+    }
+}
+#[derive(Deserialize, Getters)]
+pub struct DatabaseConfiguration {
+    name: String,
+    password: String,
+    username: String,
+}
+
+impl DatabaseConfiguration {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@postgres/{}?currentSchema=my_schema",
+            self.username, self.password, self.name
+        )
+    }
 }
 
 pub fn get_configuration() -> Result<Configuration, figment::Error> {
--- tsukimi-api/src/main.rs
+++ tsukimi-api/src/main.rs
@@ -3,30 +3,52 @@ use tokio::net::TcpListener;
 use tracing::{error, info};
 
 pub mod config;
+pub mod routes;
+pub mod services;
+
+#[derive(Clone)]
+struct AppState {
+    pub database: services::database::DatabaseService,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing_subscriber::fmt()
-        // .with_env_filter("info")
-        // .with_target(false)
-        // .with_thread_ids(true)
-        // .with_thread_names(true)
-        .init();
+    tracing_subscriber::fmt().init();
+
+    let version = env!("CARGO_PKG_VERSION");
+    let app_name = env!("CARGO_PKG_NAME");
+
+    // Load configuration
     let config = config::get_configuration().map_err(|e| {
         error!("Failed to load configuration: {}", e);
         e
     })?;
 
-    info!("Starting Tsukimi CDN on port: {}", config.port());
+    info!("{} v{}", app_name, version);
+    info!("Running in {:?} mode", config.env());
+    #[cfg(debug_assertions)]
+    info!("/!\\ Debug mode is enabled");
+
+    let (database_service) = services::get_services(&config).await.map_err(|e| {
+        error!("Failed to initialize services: {}", e);
+        e
+    })?;
+
+    let app_state = AppState {
+        database: database_service,
+    };
 
     let address = SocketAddr::from((Ipv4Addr::UNSPECIFIED, config.port()));
-
     let listener = TcpListener::bind(address).await.map_err(|e| {
         error!("Failed to bind to address {}: {}", address, e);
         e
     })?;
 
-    axum::serve(listener, axum::Router::new())
+    let router = routes::get_router().with_state(app_state);
+
+    info!("Starting Tsukimi CDN on port: {}", config.port());
+
+    axum::serve(listener, router)
         .with_graceful_shutdown(shutdown_signal())
         .await
         .map_err(|e| {
